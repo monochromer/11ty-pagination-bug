f84711c46a261e3647eaf4f909b681b00a8f0dec
monochromer <monochromer@mail.ru>
2021-10-04 15:09:15 +0500

    test

--- netlify/functions/image.js
+++ netlify/functions/image.js
@@ -1,8 +1,9 @@
+// ts-check
 const { URL, URLSearchParams } = require('url')
+const path = require('path')
 const { builder } = require('@netlify/functions')
 const chromium = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer-core')
-const crypto = require('crypto');
 
 const sizes = {
   og: {
@@ -16,29 +17,16 @@ const sizes = {
 }
 
 /**
- * @typedef {Object} Event
- * @property {string} rawUrl
- * @property {string} rawQuery
- * @property {string} path
- * @property {string} httpMethod
- * @property {EventHeaders} headers
- * @property {EventMultiValueHeaders} multiValueHeaders
- * @property {EventQueryStringParameters | null}  queryStringParameters
- * @property {EventMultiValueQueryStringParameters | null} multiValueQueryStringParameters
- * @property {string | null} body
- * @property {boolean} isBase64Encoded
+ * @type {import('@netlify/functions').Handler}
  */
-
-/**
- *
- * @param {Event} event
- * @param {object} context
- * @returns {object}
- */
-async function handler(event, context) {
+async function handler(event) {
   try {
-    const params = new URLSearchParams(event.rawQuery)
-    const imageSize = sizes[params.get('type')] || sizes.og
+    const pathSegments = event.path.split('/').filter(segment => !!segment)
+    const [tag, articleId] = pathSegments
+    const imageFileName = pathSegments[pathSegments.length - 1]
+    const imageType = path.basename(imageFileName, path.extname(imageFileName))
+    const imageSize = sizes[imageType] || sizes['og']
+    const url = new URL(`/${tag}/${articleId}/index.og.html`, 'https://doka-guide-platform-pr-413.surge.sh')
 
     const browser = await puppeteer.launch({
       args: chromium.args,
@@ -49,11 +37,8 @@ async function handler(event, context) {
       executablePath: await chromium.executablePath,
       headless: chromium.headless,
     });
-    const page = await browser.newPage();
+    const page = await browser.newPage()
     await page.setViewport(imageSize)
-    const url = new URL(`/${params.get('tag')}/${params.get('articleId')}/index.og.html`, 'https://doka-guide-platform-pr-413.surge.sh')
-    console.log('rendered url: ', url)
-    console.log('headers', event.headers)
     await page.goto(url.toString(), {
       waitUntil: 'domcontentloaded',
     });
@@ -66,15 +51,15 @@ async function handler(event, context) {
       // encoding: 'base64'
       // encoding: 'binary'
     })
+    const body = imageBuffer.toString('base64')
     await browser.close();
 
     return {
       statusCode: 200,
       heades: {
         'Content-Type': 'image/png',
-        'ETag': crypto.createHash('md5').update(imageBuffer).digest('hex')
       },
-      body: imageBuffer.toString('base64'),
+      body,
       isBase64Encoded: true
     };
   } catch(error) {
